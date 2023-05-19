90edf9e422b9a4ea840d5a69e1d7e8fa6daf07e3
dddlyt <957753243@qq.com>
2023-05-19 20:48:40 +0800

    创建随机生成区域类

---
+++ Source/GetStarted/SpawnVolume.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class GETSTARTED_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	class UBoxComponent* SpawnBox;
+
+	//要生成那类的物体
+	//创建一个AActor类的模板数组,所有继承AActor的子类都可以放入TSubclassOf
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Spawn Volume")
+	TArray<TSubclassOf<AActor>> SpwanActorClassesArray;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
