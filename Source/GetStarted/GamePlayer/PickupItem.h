64466fa1edb1ac1194013ac8679fe88f89c50fe5
dddlyt <957753243@qq.com>
2023-05-22 20:16:22 +0800

    507-创建可拾取道具的基类

---
+++ Source/GetStarted/GamePlayer/PickupItem.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GamePlayer/IneractableItem.h"
+#include "PickupItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GETSTARTED_API APickupItem : public AIneractableItem
+{
+	GENERATED_BODY()
+	
+public:
+	APickupItem();
+
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32  OtherBodyIndex) override;
+
+	UFUNCTION(BlueprintImplementableEvent,Category="Pickup")
+	void OnPickup(const class AMainPlayer* MainPlayer);
+};
