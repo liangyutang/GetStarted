c9688d568a673c4e810a05b56f4b767693ff98c7
dddlyt <957753243@qq.com>
2023-05-23 19:41:58 +0800

    620-创建并设置与追逐相关的组件

--- Source/GetStarted/Characters/Enemy/BaseEnemy.cpp
+++ Source/GetStarted/Characters/Enemy/BaseEnemy.cpp
@@ -3,18 +3,58 @@
 
 #include "Characters/Enemy/BaseEnemy.h"
 
+#include "Components/CapsuleComponent.h"
+#include "Components/SphereComponent.h"
+
 // Sets default values
 ABaseEnemy::ABaseEnemy()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	/**如果怪物挡在摄像机与玩家之间，需要将怪物处理*/
+	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+
+	ChaseVolume = CreateDefaultSubobject<USphereComponent>(TEXT("ChaseVolume"));
+	ChaseVolume->SetupAttachment(GetRootComponent());
+	ChaseVolume->InitSphereRadius(800.0f);
+	
+
+	AttackVolume = CreateDefaultSubobject<USphereComponent>(TEXT("AttackVolume"));
+	AttackVolume->SetupAttachment(GetRootComponent());
+	AttackVolume->InitSphereRadius(100.0f);
+	
 }
 
 // Called when the game starts or when spawned
 void ABaseEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//设置碰撞检测
+	//指定当前的碰撞类型
+	ChaseVolume->SetCollisionObjectType(ECC_WorldDynamic);
+	//只响应pawn，其他不响应,不知道为什么，AttackVolume->SetCollisionResponseToAllChannels(ECR_Ignore);不生效
+	ChaseVolume->SetCollisionResponseToAllChannels(ECR_Ignore);
+	ChaseVolume->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	//设置碰撞检测
+	//指定当前的碰撞类型
+	AttackVolume->SetCollisionObjectType(ECC_WorldDynamic);
+	//只响应pawn，其他不响应,不知道为什么，AttackVolume->SetCollisionResponseToAllChannels(ECR_Ignore);不生效
+	AttackVolume->SetCollisionResponseToAllChannels(ECR_Ignore);
+	AttackVolume->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	//当区域开始被重叠时
+	ChaseVolume->OnComponentBeginOverlap.AddDynamic(this, &ABaseEnemy::OnChaseVolumeOverlapBegin);
+	//当重叠事件结束时
+	ChaseVolume->OnComponentEndOverlap.AddDynamic(this, &ABaseEnemy::OnChaseVolumeOverlapEnd);
+
+	//当区域开始被重叠时
+	AttackVolume->OnComponentBeginOverlap.AddDynamic(this, &ABaseEnemy::OnAttackVolumeOverlapBegin);
+	//当重叠事件结束时
+	AttackVolume->OnComponentEndOverlap.AddDynamic(this, &ABaseEnemy::OnAttackVolumeOverlapEnd);
 	
 }
 
@@ -30,5 +70,22 @@ void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void ABaseEnemy::OnChaseVolumeOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ABaseEnemy::OnChaseVolumeOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ABaseEnemy::OnAttackVolumeOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ABaseEnemy::OnAttackVolumeOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
 
--- Source/GetStarted/Characters/Enemy/BaseEnemy.h
+++ Source/GetStarted/Characters/Enemy/BaseEnemy.h
@@ -15,6 +15,14 @@ public:
 	// Sets default values for this character's properties
 	ABaseEnemy();
 
+	//AI×·×ÙÌå»ý
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="AI")
+	class USphereComponent* ChaseVolume;
+
+	//¹¥»÷·¶Î§
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	USphereComponent* AttackVolume;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -25,5 +33,17 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	virtual void OnChaseVolumeOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnChaseVolumeOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32  OtherBodyIndex);
+
+	UFUNCTION()
+	virtual void OnAttackVolumeOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnAttackVolumeOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32  OtherBodyIndex);
 
 };
